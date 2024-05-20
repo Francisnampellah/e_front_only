54213dbcd466652e465e4459a39e203e3e492806
francisnampellah <bnampellah1@gmail.com>
2024-05-20 18:05:14 +0300

    the card

--- src/component/product_card.js
+++ src/component/product_card.js
@@ -45,23 +45,27 @@ export default function Product_card({ data }) {
           </a>
           <div class="mt-4 px-5 pb-5">
             <a href="#">
-              <h5 class="text-xl tracking-tight text-slate-900 line-clamp-1">
-                {data?.title}
+              <h5 class="text-xl tracking-tight text-slate-900 line-clamp-1 font-bold">
+                {data?.productName}
               </h5>
             </a>
             <div class="mt-2 mb-5 flex items-center justify-between">
               <p>
                 <span class="text-3xl font-bold text-slate-900">
+                  {/* ${data?.productName} */}
+                </span>
+                <span class="text-sm text-slate-900 line-through">
+                  {" "}
                   ${data?.price}
                 </span>
-                <span class="text-sm text-slate-900 line-through">$699</span>
               </p>
             </div>
-            <div
+            <a
               // onClick={() => {
               //   addToCart(data);
               //   // alert.show("Oh look, an alert!");
               // }}
+              href={"/product/" + data?.id}
               class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 cursor-pointer"
             >
               <svg
@@ -78,20 +82,8 @@ export default function Product_card({ data }) {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              {false ? (
-                <div className="flex justify-center items-center">
-                  {/* <RiseLoader
-                    color={"white"}
-                    loading={true}
-                    size={10}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                  /> */}
-                </div>
-              ) : (
-                "Add To Cart"
-              )}
-            </div>
+              Nunua Sasa
+            </a>
           </div>
         </div>
       </div>
